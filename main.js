6d5e8b418f1f0a414691b1b20454cf46ca08e9ea
vincenzovolpe <vinci792010@gmail.com>
2019-11-19 15:29:08 +0100

    Aggiustato messaggio console se il numero è divisibile per 5

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ for (var i = 1; i <= 100; i++) {
     } else if (i % 3 == 0) {
         console.log('Fizz');
     } else if (i % 5 === 0) {
-        console.log(i);
+        console.log('Buzz');
     } else {
         console.log(i);
     }
