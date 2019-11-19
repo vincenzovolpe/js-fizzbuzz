06da2df0dfc2799fe999a535fa96adf9390a707a
vincenzovolpe <vinci792010@gmail.com>
2019-11-19 15:54:59 +0100

    Caricamento versione super avanzata e compatta con operatore ternario e concatenazione di AND e OR per valutare

--- main.js
+++ main.js
@@ -17,3 +17,12 @@ for (var i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+
+/*
+// Versione super avanzata
+for(let i=0;i<100;)
+  console.log(
+    ( ++i%3 ? '' : 'fizz' ) + ( i%5 ? '' : 'buzz' ) || i
+  )
+*/
