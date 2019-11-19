cb77a469e554fc5ca8390b086d94cd7fa243478e
vincenzovolpe <vinci792010@gmail.com>
2019-11-19 15:35:03 +0100

    variante multipli di 3 e 5

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
 
 // Versione base
 for (var i = 1; i <= 100; i++) {
+    /* variante per multipli di 3 e di 5 contemporaneamente
+    if (i % 15 == 0) {
+        console.log("FizzBuzz");
+    } */
     if ((i % 3 == 0) && (i % 5 == 0)){
         console.log('FizzBuzz');
     } else if (i % 3 == 0) {
